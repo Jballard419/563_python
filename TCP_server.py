96e769bb70fc92f26ed570bf57e8e6107f7d5bd6
James Ballard <j.lb4@live.com>
2016-10-19 10:59:49 -0500

    fixed bugs

--- TCP_server.py
+++ TCP_server.py
@@ -1,10 +1,10 @@
 import socket
 import sys
 TCP_IP = '127.0.0.1'
-TCP_PORT = sys.agrv[1];
+TCP_PORT = int(sys.argv[1]);
 BUFFER_SIZE= 1024
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-S.bind((TCP_IP,TCP_PORT))
+s.bind((TCP_IP,TCP_PORT))
 while 1:
     s.listen(1)
     conn,addr=s.accept()
@@ -12,6 +12,6 @@ while 1:
     while 1:
         data = conn.recv(BUFFER_SIZE)
         if not data: break
-        print "received data:", data
+        print 'received data:', data
         conn.send(data.upper());
     conn.close() #fin
--- UDP_server.py
+++ UDP_server.py
@@ -1,13 +1,14 @@
 import socket
 import sys
 UDP_IP="127.0.0.1"
-UDP_PORT = sys.argv[1]
-sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+UDP_PORT =int( sys.argv[1])
+sock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP,UDP_PORT))
 
 while 1:
     data, addr =sock.recvfrom(1024)
     print "received message:", data
-    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.sendto(data.upper(), (addr, UDP_PORT))
+   
+    sock.sendto(data.upper(), addr )
+
     
