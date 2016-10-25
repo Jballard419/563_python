4d06a3cfbabd7797675b491d35910b1a36c79427
James Ballard <j.lb4@live.com>
2016-10-24 22:13:41 -0500

    st

--- TCP_server.py
+++ TCP_server.py
@@ -4,7 +4,7 @@ TCP_IP = '127.0.0.1'
 TCP_PORT = int(sys.argv[1]);
 BUFFER_SIZE= 1024
 s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP,TCP_PORT))
+s.bind((TCP_PORT))
 while 1:
     s.listen(1)
     conn,addr=s.accept()
--- UDP_server.py
+++ UDP_server.py
@@ -1,5 +1,7 @@
 import socket
 import sys
+local_ip= socket.gethostbyname(socket.gethostname())
+print "local address:", local_ip
 UDP_IP="127.0.0.1"
 UDP_PORT =int( sys.argv[1])
 sock= socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -8,7 +10,5 @@ sock.bind((UDP_IP,UDP_PORT))
 while 1:
     data, addr =sock.recvfrom(1024)
     print "received message:", data
-   
-    sock.sendto(data.upper(), addr )
 
-    
+    sock.sendto(data.upper(), addr )
